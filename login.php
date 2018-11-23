d58342ee85d0cca392bd4a4647e24086ffa82af7
AniketPatel13 <42162619+AniketPatel13@users.noreply.github.com>
2018-11-23 10:32:42 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<h3>Add entry</h3>
+<p> Add another Article</p>
+<form action="login.php" method = "post">
+  <label for="username">Username</label> <input type="username" id="usename" name="username"><br /><br />
+  <label for="password">Password:</label> <input type="text" id="password" name="password"><br /><br />
+  <button type = "submit">Login</button>
+</form>
+</html>
