c57a9b4f5ff2f06499ceb0e9e896c9ca99e20bc8
Chinu Sohlot <65461295+YoungSTAAR@users.noreply.github.com>
2022-04-28 17:53:25 +0530

    insert body  tag

--- login.php
+++ login.php
@@ -3,9 +3,11 @@
 <head>
   <title>Login</title>
 </head>
+<body>
 <form action="login.php" method = "post">
   <label for="username">Username</label> <input type="username" id="usename" name="username"><br /><br />
   <label for="password">Password:</label> <input type="text" id="password" name="password"><br /><br />
   <button type = "submit">Login</button>
 </form>
+</body>
 </html>
